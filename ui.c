97a09012519098d8bc58b1eade91681253f7083b
Jan Goličnik <jan@nejka.net>
2026-08-16 22:34:27 +0200

    asd

--- ui.c
+++ ui.c
@@ -2,9 +2,9 @@
 #include "base.c"
 
 void ui_init(void) {
-#ifdef __EMSCRIPTEN__
-    ripple_emscripten_init(&renderer.ripple_context, window.window, false);
-#else // __EMSCRIPTEN__
-    ripple_glfw_init(&renderer.ripple_context, window.window, false);
-#endif // __EMSCRIPTEN__
+// #ifdef __EMSCRIPTEN__
+//     ripple_emscripten_init(&renderer.ripple_context, window.window, false);
+// #else // __EMSCRIPTEN__
+//     ripple_glfw_init(&renderer.ripple_context, window.window, false);
+// #endif // __EMSCRIPTEN__
 }
--- window.c
+++ window.c
@@ -179,19 +179,19 @@ void window_update_input(Allocator* allocator) {
         window.keys[KEY_RELEASED][i] = false;
     }
 
-    if (!CURSOR().consumed && CURSOR().left.pressed) {
+    // if (!CURSOR().consumed && CURSOR().left.pressed) {
     #ifdef __EMSCRIPTEN__
         emscripten_request_pointerlock("#canvas", EM_TRUE);
     #else
         glfwSetInputMode(window.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         window.mouse.has_lock = true;
     #endif
-    }
+    // }
 
-    text(mrw_format("mousedxdy: {.2f} {.2f}", allocator,
-        window.mouse.dx,
-        window.mouse.dy
-    ));
+    // text(mrw_format("mousedxdy: {.2f} {.2f}", allocator,
+    //     window.mouse.dx,
+    //     window.mouse.dy
+    // ));
     window.mouse.dx = 0.0f;
     window.mouse.dy = 0.0f;
 }
